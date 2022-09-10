04fa500ffe4f637686bfe0202f0af352b6edecfb
hoshbosh <jlabasbas24@gmail.com>
2022-09-10 17:46:10 -0400

    Text Color Change

--- littergo/src/components/Profile.js
+++ littergo/src/components/Profile.js
@@ -3,7 +3,7 @@ export default function Player(){
     
     return(
         <div className="col-span-2 row-span-3 grid grid-rows-2 bg-slate-500 p-5">
-            <img className='pic justify-self-center' src={pfp} />
+            <img className='pic justify-self-center' src={pfp}  alt="Cleanup Conquest Logo"/>
             <div>
             <h1 className='font-LilitaOne text-xl'>
                 LVL. 1
