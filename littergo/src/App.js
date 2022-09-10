f6778cde4beda22eb5d724b8ad895767fbae93f0
hoshbosh <jlabasbas24@gmail.com>
2022-09-10 17:33:37 -0400

    More Progress

--- littergo/src/App.js
+++ littergo/src/App.js
@@ -3,7 +3,7 @@ import TitleCard from './components/TitleCard';
 import Player from './components/Profile';
 function App() {
   return (
-    <div className='p-5 grid grid-cols-2 grid-rows-5 border-slate-0 h-screen bg-slate-500'>
+    <div className='p-8 grid grid-cols-2 grid-rows-5 border-slate-0 h-screen bg-slate-500'>
       <TitleCard />
       <Player/> 
       <button className='btn'>
