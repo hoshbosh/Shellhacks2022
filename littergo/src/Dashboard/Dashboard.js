a96d5aca291fec810faa32148c763f3aeaad17a1
hoshbosh <jlabasbas24@gmail.com>
2022-09-11 09:54:35 -0400

    Minor Changes to Button Styling

--- littergo/src/Dashboard/Dashboard.js
+++ littergo/src/Dashboard/Dashboard.js
@@ -23,7 +23,7 @@ export function Dashboard() {
       <TitleCard />
       <Player progress={exp} function={resetCallback}/> 
       <AddLitterButton function={addCallback}/>
-          <Link to="/Leaderboard" className='btn text-center p-8 inset-y-4'>
+          <Link to="/Leaderboard" className='btn text-center p-8 p-y-10'>
                 Leaderboard
           </Link>
     </div>
