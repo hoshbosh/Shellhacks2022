cec252d3dc7d0a6b70d33b55fcb39605c4d37763
hoshbosh <jlabasbas24@gmail.com>
2022-09-11 10:02:59 -0400

    Padding

--- littergo/src/Dashboard/Dashboard.js
+++ littergo/src/Dashboard/Dashboard.js
@@ -23,7 +23,7 @@ export function Dashboard() {
       <TitleCard />
       <Player progress={exp} function={resetCallback}/> 
       <AddLitterButton function={addCallback}/>
-          <Link to="/Leaderboard" className='btn text-center p-y-14 h-full w-full'>
+          <Link to="/Leaderboard" className='btn text-center py-14 h-full w-full'>
                 Leaderboard
           </Link>
     </div>
